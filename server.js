ec6ed33f5e4b563f21887360b75c138dd3f1769f
Laécio Rodrigues <37408950+laecioandrade@users.noreply.github.com>
2022-02-14 20:30:25 -0300

    Update server.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const port = 3000;
 
-app.listen(3000)
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
