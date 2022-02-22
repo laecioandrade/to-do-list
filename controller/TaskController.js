d001c2e855e829c6c10cbbbbb1ddd3075acd5660
Laécio Rodrigues <37408950+laecioandrade@users.noreply.github.com>
2022-02-22 15:38:17 -0300

    Configuring check in the database.

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -76,3 +76,14 @@ exports.deleteOneTask = async (req, res) => {
     res.status(500).send({ error: err.message });
   }
 };
+
+exports.taskCheck = async (req, res) => {
+  try {
+    const task = await Task.findOne({_id: req.params.id});
+    task.check ? task.check = false : task.check = true;
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({error: err.message})
+  }
+}
